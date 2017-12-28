9d3b596a2955b48d74f0e96106d737b86ab8891c
Exmintor <robertsmbj@gmail.com>
2017-12-28 18:18:04 +0000

    Basic Ability Designer
    Adds more functionality to the Ability Designer.

---
+++ Assets/Scripts/AbilityDesigner/AbilityDesigner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityDesigner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject abilityPrefab;
+    [SerializeField]
+    private Transform abilityListPanel;
+
+    private AbilityList list;
+	// Use this for initialization
+	void Start ()
+    {
+        list = new AbilityList();
+        InitializeMenuItems();
+	}
+
+    private void InitializeMenuItems()
+    {
+        foreach(Ability abil in list.Abilities)
+        {
+            GameObject newAbility = Instantiate(abilityPrefab, abilityListPanel) as GameObject;
+            newAbility.GetComponent<Toggle>().group = abilityListPanel.GetComponent<ToggleGroup>();
+            newAbility.GetComponentInChildren<Text>().text = abil.Name;
+            newAbility.GetComponent<AbilityDesignerItem>().AbilityRepresented = abil;
+        }
+    }
+
+    public void UpdateEditFields()
+    {
+
+    }
+}
---
+++ Assets/Scripts/AbilityDesigner/AbilityDesignerItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityDesignerItem : MonoBehaviour
+{
+    private AbilityDesigner designer;
+
+    public Ability AbilityRepresented { get; set; }
+
+	// Use this for initialization
+	void Start ()
+    {
+        designer = FindObjectOfType<AbilityDesigner>();
+	}
+
+    public void AbilityWasClicked()
+    {
+        designer.UpdateEditFields();
+    }
+}
