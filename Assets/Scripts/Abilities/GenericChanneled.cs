58d621cd9740e83d1fa445456795972d690834b8
Exmintor <robertsmbj@gmail.com>
2018-01-02 18:06:14 +0000

    Adds Abilities
    Adds a basic framework for Abilities to the project.

---
+++ Assets/Scripts/Abilities/GenericChanneled.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+/// <summary>
+/// A Generic Channeled Ability. It doesn't define any Cast() method.
+/// TODO: Get rid of this class. It only serves as an example of how
+/// the chain of inheritance works.
+/// </summary>
+public class GenericChanneled : ChanneledAbility
+{
+    public GenericChanneled()
+    {
+        // Default values.
+        // Can be overridden in Json after it has been saved,
+        // or by using the Ability Designer.
+        ID = 100;
+        Name = "GenericChanneled";
+        ImageFilePath = "GenericChanneled";
+        Description = "This is a generic channeled ability";
+        Cooldown = 0.0f;
+        ChannelTime = 0.0f;
+    }
+
+    public override void Cast()
+    {
+        throw new NotImplementedException();
+    }
+}
